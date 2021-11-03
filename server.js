ffd174b09e62527b003817fd7a772bcab737ed5e
prashanta-sam-velectico <prashanta.das@velectico.com>
2021-11-03 18:49:06 +0530

    added server js file

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+
+app.use(express.static(path.resolve(__dirname, './public')));
+app.get('*', async function (req, res) { 
+
+const filePath = path.resolve(__dirname, './public', 'index.html');
+res.sendFile(filePath);
+
+});
+
+app.listen(8080, function () {
+console.log('Example app listening on port 8080!');
+});
